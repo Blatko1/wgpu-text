f0ec9168043acdb9cd24d5b0e4fe3b85a9dcd152
Blatko1 <kings.of.funny.gemplays@gmail.com>
2022-03-20 18:19:09 +0100

    Build fix

--- src/lib.rs
+++ src/lib.rs
@@ -3,29 +3,8 @@
 //! This project was inspired by and is similar to [wgpu_glyph](https://github.com/hecrj/wgpu_glyph), but has additional features and is simpler. Also there is no need to include glyph-brush in your project.
 //! 
 //! Some features are directly implemented from glyph-brush so you should go trough [Section docs](https://docs.rs/glyph_brush/latest/glyph_brush/struct.Section.html) for better understanding of adding and managing text.
-//! 
-//! Example:
-//! ```rust
-//! use wgpu_text::BrushBuilder;
-//! use wgpu_text::section::{Section, Text, Layout, HorizontalAlign};
-//! let brush = BrushBuilder::using_font_bytes(font).unwrap().build(
-//!         &device, format, width, height);
-//! let section = Section::default()
-//!     .add_text(Text::new("Hello World"))
-//!     .with_layout(Layout::default().h_align(HorizontalAlign::Center));
-//! 
-//! // window event loop:
-//!     winit::event::Event::RedrawRequested(_) => {
-//!         // Has to be queued every frame.
-//!         brush.queue(&section);
-//!         let cmd_buffer = brush.draw_queued(&device, &view, &queue);
-//!         // Has to be submitted last so text won't be overlapped.
-//!         queue.submit(cmd_buffer);
-//!         frame.submit();
-//!     }
-//! ```
-//! 
-//! * Look trough [examples](https://github.com/Blatko1/wgpu_text/tree/master/examples) for more.
+//!
+//! * Look trough [examples](https://github.com/Blatko1/wgpu_text/tree/master/examples).
 
 mod brush;
 mod pipeline;
